9b7889540fc78786af1c3fdaacd086bf72d94135
Roberto Achar <robertoachar@gmail.com>
2017-04-24 16:27:31 -0300

    Improving .editorconfig

--- index.js
+++ index.js
@@ -14,46 +14,46 @@ const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01
  * Parse
  */
 function parse(address, cidr, done) {
-    if (!isString(address)) {
-        return done('Address is not a string');
-    }
+  if (!isString(address)) {
+    return done('Address is not a string');
+  }
 
-    var match = address.match(regex);
+  var match = address.match(regex);
 
-    if (!match) {
-        return done('Invalid address');
-    }
+  if (!match) {
+    return done('Invalid address');
+  }
 
-    if (!isNumber(cidr)) {
-        return done('Cidr is not a number');
-    }
+  if (!isNumber(cidr)) {
+    return done('Cidr is not a number');
+  }
 
-    if (cidr < 0 || cidr > 32) {
-        return done('Invalid cidr');
-    }
+  if (cidr < 0 || cidr > 32) {
+    return done('Invalid cidr');
+  }
 
-    var ipv4 = new IPv4(address, cidr);
+  var ipv4 = new IPv4(address, cidr);
 
-    if (done) return done(null, ipv4);
+  if (done) return done(null, ipv4);
 }
 
 /**
  * isNumber
  */
 function isNumber(value) {
-    if (typeof value !== 'number') return false;
+  if (typeof value !== 'number') return false;
 
-    return true;
+  return true;
 }
 
 /**
  * isString
  */
 function isString(value) {
-    if (typeof value !== 'string') return false;
-    if (value.trim() === '') return false;
+  if (typeof value !== 'string') return false;
+  if (value.trim() === '') return false;
 
-    return true;
+  return true;
 }
 
 /**
