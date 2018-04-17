8ec1e57f5934aa2f40cb61d275aecbdaf07bb9ff
Roberto Achar <robertoachar@gmail.com>
2018-04-17 20:54:02 -0300

    refactor: remove unused code

--- sample/sample.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-console: 0 */
-
-const lib = require('../src/index');
-
-lib.parse('192.168.1.1', 32, (err, subnet) => {
-  if (err) return console.error(err);
-
-  console.log(subnet);
-});
